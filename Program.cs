769b4dce1099e63f1509085e9b0989e2d2fe5e19
Paulo Victor Lorençato <pvlorencato@gmail.com>
2025-10-09 23:54:05 -0300

    feat/incluídos atributos faltantes na classe pai, herança nas classes filhas e sequência de testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var iphone = new Iphone("11922446688", "iPhone 14", "IMEI123456789", 2048);
+var nokia = new Nokia("11988664422", "Nokia Phone", "IMEI987654321", 1024);
+
+// Testa métodos herdados no Iphone
+Console.WriteLine($"Testando funcionalidades {iphone.Modelo}");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+// Testa métodos herdados no Nokia
+Console.WriteLine($"Testando funcionalidades {nokia.Modelo}");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
